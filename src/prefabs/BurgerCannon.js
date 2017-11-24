e166fce7e7dcc137493b9934c1782e37fa1399fa
Алексей Протасов <a.protasov@corp.mail.ru>
2017-11-24 23:26:18 +0300

    Add shooting cannons

---
+++ src/prefabs/BurgerCannon.js
@@ -0,0 +1,34 @@
+const BULLET_SPEED = 100;
+const FIRE_RATE = 150;
+
+export default class BurgerCannon extends Phaser.TileSprite {
+  constructor(game, x, y) {
+    super(game, x, y, 21, 21, 'game_tiles', 858);
+
+    game.physics.arcade.enable(this);
+    this.body.allowGravity = false;
+    this.body.immovable = true;
+
+    this.weapon = this.__createWeapon();
+
+    game.time.events.loop(
+      game.rnd.integerInRange(1500, 2000),
+      () => this.weapon.fire()
+    );
+  }
+  getWeapon() {
+    return this.weapon;
+  }
+  __createWeapon() {
+    const weapon = this.game.add.weapon(20, 'game_tiles', 627);
+    weapon.bulletKillType = Phaser.Weapon.KILL_WORLD_BOUNDS;
+    weapon.bulletKillDistance = 630;
+    weapon.bulletSpeed = BULLET_SPEED;
+    weapon.fireAngle = -180;
+    weapon.autofire = false;
+    weapon.bulletGravity.y = -1000;
+    weapon.trackSprite(this, -10, 5, false);
+
+    return weapon;
+  }
+}
--- src/states/Game.js
+++ src/states/Game.js
@@ -1,19 +1,28 @@
 import Player from '../prefabs/Player';
+import BurgerCannon from '../prefabs/BurgerCannon';
 
 export default class GameState extends Phaser.State {
   init() {
     this.game.physics.arcade.gravity.y = 1000;
+
+    this.cannons = this.game.add.group(this, 'cannons', true, true);
   }
   create() {
     this.__createLevel();
     this.__createPlayer();
+    this.cannons = this.__createCannons();
+    this.cannonBullets = this.cannons.children.map((cannon) => cannon.getWeapon().bullets);
   }
   update() {
     this.game.physics.arcade.collide(this.player, this.collisionLayer);
+    this.game.physics.arcade.overlap(this.player, this.cannonBullets, this.__touchCannon);
   }
   render(game) {
     // TODO Add debug logic here if needed
     //this.game.debug.body(this.player);
+  }
+  __touchCannon(player, bullet) {
+    bullet.kill();
   }
   __createLevel() {
     this.map = this.add.tilemap('playground_level');
@@ -28,12 +37,21 @@ export default class GameState extends Phaser.State {
     this.player = this.game.add.existing(new Player(this.game, 0, 42));
     this.game.camera.follow(this.player);
   }
-  __findObjectsByType(targetType, map, layer) {
-    return map.objects[layer].reduce((acc, obj) => {
+  __createCannons() {
+    const cannons = this.__findObjectsByType('burger_cannon');
+    console.log('>>>', cannons);
+    const group = this.game.add.group();
+    cannons.forEach((item) =>
+      group.add(new BurgerCannon(this.game, item.x, item.y))
+    );
+    return group;
+  }
+  __findObjectsByType(targetType) {
+    return this.map.objects['objects'].reduce((acc, obj) => {
       if (obj.type === targetType) {
         return acc.concat({
           ...obj,
-          y: obj.y - map.tileHeight
+          y: obj.y - this.map.tileHeight
         });
       }
       return acc;
