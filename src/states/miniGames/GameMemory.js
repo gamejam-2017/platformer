4b4d420684fb1f2fab73fe13242c751cab0b3e67
Anashkin Aleksandr <a.anashkin@corp.mail.ru>
2017-11-25 18:12:27 +0300

    paddings

--- src/states/miniGames/GameMemory.js
+++ src/states/miniGames/GameMemory.js
@@ -7,90 +7,90 @@ export default ({
   levelName,
   onNext
 }) => class GameMemory extends Phaser.State {
-  init() {
-    this.firstTile = null;
-    this.secondTile = null;
-    this.canPick = true;
-    this.game.stage.backgroundColor = '#888888';
-  }
-  create(game) {
-    let findTilesCount = 0;
-    // choose 8 random tile images
-    let chosenTiles = [];
-    while(chosenTiles.length<16){
-      let candidate=Math.floor(Math.random()*8);
-      if(chosenTiles.indexOf(candidate) === -1){
-        chosenTiles.push(candidate,candidate)
+    init() {
+      this.firstTile = null;
+      this.secondTile = null;
+      this.canPick = true;
+      this.game.stage.backgroundColor = '#888888';
+    }
+    create(game) {
+      let findTilesCount = 0;
+      // choose 8 random tile images
+      let chosenTiles = [];
+      while (chosenTiles.length < 16) {
+        let candidate = Math.floor(Math.random() * 8);
+        if (chosenTiles.indexOf(candidate) === -1) {
+          chosenTiles.push(candidate, candidate)
+        }
       }
-    }
-    // shuffle the chosen tiles
-    for(let i=0;i<32;i++){
-      let from = Math.floor(Math.random()*16);
-      let to = Math.floor(Math.random()*16);
-      [chosenTiles[from], chosenTiles[to]] = [chosenTiles[to], chosenTiles[from]];
-    }
-    console.log(chosenTiles);
+      // shuffle the chosen tiles
+      for (let i = 0; i < 32; i++) {
+        let from = Math.floor(Math.random() * 16);
+        let to = Math.floor(Math.random() * 16);
+        [chosenTiles[from], chosenTiles[to]] = [chosenTiles[to], chosenTiles[from]];
+      }
+      console.log(chosenTiles);
 
-    for(let i = 0;i < 4; ++i){
-      for(let j = 0;j < 4; ++j){
-        let tile = this.game.add.tileSprite(9+i*23, 9+j*23, 21, 21, 'game_tiles', DEFAULT_FRAME);
-        tile.buttonMode = true;
-        tile.inputEnabled  = true;
-        tile.interactive = true;
-        tile.isSelected = false;
-        tile.theVal = 2 + chosenTiles[i*4+j]*2;
-        tile.alpha = 0.5;
-        tile.events.onInputDown.add((data) => {
-          if(this.canPick) {
-            if(!data.isSelected){
-              data.frame = data.theVal;
-              data.isSelected = true;
-              data.alpha = 1;
-              if(this.firstTile === null){
-                this.firstTile=data
-              }
-              else{
-                this.secondTile=data;
-                this.canPick=false;
-                if(this.firstTile.theVal === this.secondTile.theVal){
-                  setTimeout(() => {
-                    findTilesCount += 2;
-                    this.firstTile.alpha=0;
-                    this.secondTile.alpha=0;
-                    this.firstTile=null;
-                    this.secondTile=null;
-                    this.canPick=true;
-                    if (findTilesCount === 16) {
-                      storage.setItem(levelName, {
-                        isDone: true,
-                        // TODO можно добавить время прохождения
-                        time: ''
-                      });
-                      onNext(true);
-                    }
-                  },500);
+      for (let i = 0; i < 4; ++i) {
+        for (let j = 0; j < 4; ++j) {
+          let tile = this.game.add.tileSprite(9 + i * 23, 9 + j * 23, 21, 21, 'game_tiles', DEFAULT_FRAME);
+          tile.buttonMode = true;
+          tile.inputEnabled = true;
+          tile.interactive = true;
+          tile.isSelected = false;
+          tile.theVal = 2 + chosenTiles[i * 4 + j] * 2;
+          tile.alpha = 0.5;
+          tile.events.onInputDown.add((data) => {
+            if (this.canPick) {
+              if (!data.isSelected) {
+                data.frame = data.theVal;
+                data.isSelected = true;
+                data.alpha = 1;
+                if (this.firstTile === null) {
+                  this.firstTile = data
                 }
-                else{
-                  setTimeout(() => {
-                    this.firstTile.isSelected=false;
-                    this.secondTile.isSelected=false;
-                    this.firstTile.frame = DEFAULT_FRAME;
-                    this.secondTile.frame = DEFAULT_FRAME;
-                    this.firstTile.alpha=0.5;
-                    this.secondTile.alpha=0.5;
-                    this.firstTile=null;
-                    this.secondTile=null;
-                    this.canPick=true;
-                  },500);
+                else {
+                  this.secondTile = data;
+                  this.canPick = false;
+                  if (this.firstTile.theVal === this.secondTile.theVal) {
+                    setTimeout(() => {
+                      findTilesCount += 2;
+                      this.firstTile.alpha = 0;
+                      this.secondTile.alpha = 0;
+                      this.firstTile = null;
+                      this.secondTile = null;
+                      this.canPick = true;
+                      if (findTilesCount === 16) {
+                        storage.setItem(levelName, {
+                          isDone: true,
+                          // TODO можно добавить время прохождения
+                          time: ''
+                        });
+                        onNext(true);
+                      }
+                    }, 500);
+                  }
+                  else {
+                    setTimeout(() => {
+                      this.firstTile.isSelected = false;
+                      this.secondTile.isSelected = false;
+                      this.firstTile.frame = DEFAULT_FRAME;
+                      this.secondTile.frame = DEFAULT_FRAME;
+                      this.firstTile.alpha = 0.5;
+                      this.secondTile.alpha = 0.5;
+                      this.firstTile = null;
+                      this.secondTile = null;
+                      this.canPick = true;
+                    }, 500);
+                  }
                 }
               }
             }
-          }
-        });
+          });
+        }
       }
     }
   }
-}
 
 
 
--- src/states/miniGames/GameSpeed.js
+++ src/states/miniGames/GameSpeed.js
@@ -7,63 +7,63 @@ export default ({
   levelName,
   onNext
 }) => class GameSpeed extends Phaser.State {
-  init() {
-    this.game.stage.backgroundColor = '#000066';
-    this.colors = [0xFFFF0B, 0xFF700B, 0x4286f4, 0x4286f4, 0xf441e8, 0x8dff6d, 0x41ccc9, 0xe03375, 0x95e032, 0x77c687, 0x43ba5b, 0x0ea3ba];
-    this.figuresAmount = -1;
-    this.radius = 50;
-    this.inAreaX = this.game.width - 2*this.radius;
-    this.score = 0;
-    this.win = false;
-  }
-  create(game) {
-    setInterval(this.drawCircle, 1000);
-  }
-  render() {
-    if (!this.win) {
-      if (this.score >= SCORE_TO_WIN) {
-        this.game.debug.text('Вы выиграли', 10, 20);
-        storage.setItem(levelName, {
-          isDone: true,
-          // TODO можно добавить время прохождения
-          time: ''
-        });
-        onNext(true);
-      } else {
-        this.game.debug.text('Счет: ' + this.score, 10, 20);
+    init() {
+      this.game.stage.backgroundColor = '#000066';
+      this.colors = [0xFFFF0B, 0xFF700B, 0x4286f4, 0x4286f4, 0xf441e8, 0x8dff6d, 0x41ccc9, 0xe03375, 0x95e032, 0x77c687, 0x43ba5b, 0x0ea3ba];
+      this.figuresAmount = -1;
+      this.radius = 50;
+      this.inAreaX = this.game.width - 2 * this.radius;
+      this.score = 0;
+      this.win = false;
+    }
+    create(game) {
+      setInterval(this.drawCircle, 1000);
+    }
+    render() {
+      if (!this.win) {
+        if (this.score >= SCORE_TO_WIN) {
+          this.game.debug.text('Вы выиграли', 10, 20);
+          storage.setItem(levelName, {
+            isDone: true,
+            // TODO можно добавить время прохождения
+            time: ''
+          });
+          onNext(true);
+        } else {
+          this.game.debug.text('Счет: ' + this.score, 10, 20);
+        }
       }
     }
-  }
-  drawCircle = () => {
-    let rand = Math.floor(Math.random() * this.colors.length);
-    let circleX = Math.floor(Math.random() * this.inAreaX);
-    const graphics = this.game.add.graphics(0, 0);
-    graphics.beginFill(this.colors[rand]);
-    const circle = graphics.drawCircle(circleX, -this.radius, this.radius);
-    graphics.endFill();
+    drawCircle = () => {
+      let rand = Math.floor(Math.random() * this.colors.length);
+      let circleX = Math.floor(Math.random() * this.inAreaX);
+      const graphics = this.game.add.graphics(0, 0);
+      graphics.beginFill(this.colors[rand]);
+      const circle = graphics.drawCircle(circleX, -this.radius, this.radius);
+      graphics.endFill();
 
-    circle.interactive = true;
-    circle.inputEnabled = true;
-    circle.buttonMode = true;
-    circle.live = true;
-    this.figuresAmount++;
-    circle.num = this.figuresAmount;
-    this.game.add.tween(circle)
-      .to({y: this.game.height + this.radius*2}, 2000, Phaser.Easing.Linear.None, true)
-      .onComplete.add(() => {
-        if (circle.live && !this.win) {
-          this.score -= 5;
+      circle.interactive = true;
+      circle.inputEnabled = true;
+      circle.buttonMode = true;
+      circle.live = true;
+      this.figuresAmount++;
+      circle.num = this.figuresAmount;
+      this.game.add.tween(circle)
+        .to({ y: this.game.height + this.radius * 2 }, 2000, Phaser.Easing.Linear.None, true)
+        .onComplete.add(() => {
+          if (circle.live && !this.win) {
+            this.score -= 5;
+          }
+        });
+      circle.events.onInputDown.add((data) => {
+        if (data.live) {
+          data.live = false;
+          data.alpha = 0;
+          ++this.score;
         }
       });
-    circle.events.onInputDown.add((data) => {
-      if (data.live) {
-        data.live = false;
-        data.alpha = 0;
-        ++this.score;
-      }
-    });
-  };
-}
+    };
+  }
 
 
 
